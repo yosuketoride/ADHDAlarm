e610e93ce8ac449b9d05235b9ed08408112a33d6
Yosuke Toride <yosuketoride@gmail.com>
2026-03-16 20:29:09 +0900

    Initial Commit

---
+++ ADHDAlarm/ADHDAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADHDAlarmApp.swift
+//  ADHDAlarm
+//
+//  Created by 取出洋介 on 2026/03/16.
+//
+
+import SwiftUI
+
+@main
+struct ADHDAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
